833df61b2253d002157a7e7708c6c2dbb8ea06e7
PuniCharana <punicharana@gmail.com>
2019-04-10 16:12:17 +0530

    setup migration

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import exc
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 db = SQLAlchemy()
+migrate = Migrate(app, db)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://punicharana:password@localhost:5432/template1'
 db.init_app(app)
